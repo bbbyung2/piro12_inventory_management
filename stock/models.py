490d1da3d18c1d2f57d41b511cd32e0f6b7c9316
bbbyung2 <bhko0524@naver.com>
2020-01-28 12:07:56 +0900

    inventory_management

--- stock/models.py
+++ stock/models.py
@@ -9,14 +9,13 @@ class Company(models.Model):
         return self.name
     
 
-
 class Stock(models.Model):
     name = models.CharField(max_length=200, verbose_name='제품명')
     img = models.ImageField(blank=True, upload_to='images/', verbose_name='제품사진')
     info = models.TextField(blank=True, verbose_name='제품설명')
     price = models.PositiveIntegerField(verbose_name='가격')
     amount = models.IntegerField(verbose_name='남은수량')
-    company = models.ForeignKey('Company', on_delete=models.CASCADE, verbose_name='거래처')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='거래처', related_name='item')
 
     def __str__(self):
         return self.name
